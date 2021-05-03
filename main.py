5c07f9a00100deb85b2e4e3351e2536d69fabf66
Emmanuel Castillo <>
2021-05-03 21:12:45 +0000

    1.2

--- main.py
+++ main.py
@@ -11,8 +11,18 @@ age_months = (round((90 * 12) - data_age * 12))
 
 print(f"You have {age_days}, {age_weeks} and {age_months} left.")
 
+#My teacher's way.
 
+# age_as_int = int(age)
 
+# years_remaining = 90 - age_as_int
+# days_remaining = years_remaining * 365
+# weeks_remaining = years_remaining * 52
+# months_remaining = years_remaining *12
+
+# message = (f"You have {days_remaining}, {weeks_remaining} and {months_remaining} left.")
+
+# print(message)
 
 
 
