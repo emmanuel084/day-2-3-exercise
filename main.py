234c1e9cb06dc706f9be09dcead49f064e49baed
Emmanuel Castillo <>
2021-05-03 20:55:29 +0000

    Life in weeks. Finished.

--- main.py
+++ main.py
@@ -3,6 +3,16 @@ age = input("What is your current age?")
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
+data_age = int(age)
+
+age_days = (round((90 * 365) - data_age * 365))
+age_weeks = (round((90 * 52) - data_age * 52))
+age_months = (round((90 * 12) - data_age * 12))
+
+print(f"You have {age_days}, {age_weeks} and {age_months} left.")
+
+
+
 
 
 
